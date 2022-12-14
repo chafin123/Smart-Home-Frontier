8c69e2ed87be84293a6a527ae2c8c08d2345041a
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2022-12-14 16:05:12 -0600

    Big change

--- web/client.js
+++ web/client.js
@@ -4,5 +4,5 @@ export default sanityClient({
   projectId: "9zwfqxw3", // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   apiVersion: '2022-10-02',
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: false // `false` if you want to ensure fresh data
 })
