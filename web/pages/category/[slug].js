5336b385b2e1a49114dad3b36220e2fc44a48c19
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2022-12-20 11:56:34 -0600

    Update [slug].js

--- web/pages/category/[slug].js
+++ web/pages/category/[slug].js
@@ -11,7 +11,7 @@ export async function getStaticPaths() {
   console.log(client);
   return {
     paths: paths.map((path) => ({params: {path}})),
-    fallback: false,
+    fallback: true,
     
   } 
 }
