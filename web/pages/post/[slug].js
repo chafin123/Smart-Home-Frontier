fa61752299cc168cba768b200c0d40a02a36bddd
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2022-12-22 21:12:08 -0600

    trying to fix getstaticprops error

--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -31,6 +31,7 @@ export async function getStaticProps(context) {
   const { slug = "" } = context.params
   try {
     post = await client.fetch(query, { slug })
+    console.log(post)
   } catch (err) { };
   return {
     props: {
